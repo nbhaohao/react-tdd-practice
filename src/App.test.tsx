ca90d8662d1ef24e1a897c2802e3924676b396ef
zhangzh <zhangzh@cnlemon.net>
2019-09-16 22:26:49 +0800

    feat: 配置 enzyme、jest-enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+it("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  const appContainerElement = wrapper.find("[data-test='app-container']");
+  expect(appContainerElement).toExist();
+  expect(appContainerElement).toHaveProp("title", "zzh");
+  expect(appContainerElement).toMatchSnapshot();
 });
